737fce6136afa1c91e91769796fcb0e17e3d5928
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-07-28 00:43:03 +0300

    feat(system): implement run system

--- src/system.cpp
+++ src/system.cpp
@@ -1,4 +1,13 @@
 #include "../include/system.hpp"
+#include "../include/actions.hpp"
+#include "../include/menus.hpp"
+#include <iostream>
+
+const int NUM_OF_SYS_ACTIONS = 5;
+int (*SYS_ACTIONS[])(Specialization *specializations[],
+                     int num_of_specializations) = {
+    nullptr, add_patient_action, pick_patient_action, print_patients_action,
+    exit_action};
 
 int init_specializations(std::string specialization_names[],
                          Specialization *specializations[],
@@ -9,4 +18,16 @@ int init_specializations(std::string specialization_names[],
                                                    specialization_capacity);
     }
     return 0;
+}
+
+int run_system(Specialization *specializations[], int num_of_specializations) {
+    int choice = main_menu();
+
+    if (choice <= 0 or choice >= NUM_OF_SYS_ACTIONS) {
+        std::cout << "Invalid choice\n";
+        return -1;
+    }
+
+    // Call the selected action function
+    return SYS_ACTIONS[choice](specializations, num_of_specializations);
 }
