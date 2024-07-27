c45e2ef6f777940e414bc8aae286e14c9692b7ed
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-07-28 00:21:07 +0300

    feat(menus): implement specialization menu

--- src/menus.cpp
+++ src/menus.cpp
@@ -10,8 +10,28 @@ int main_menu() {
     std::cout << "4) Exit\n";
 
     try {
+        std::cout << "\n> ";
         int choice;
         std::cin >> choice;
+    } catch (const std::exception &e) {
+        return -1;
+    }
+}
+
+int specialization_menu(Specialization specializations[],
+                        int num_of_specializations) {
+    std::cout << "Specializations, enter your choice:\n";
+    for (int i = 0; i < num_of_specializations; i++) {
+        std::cout << i + 1 << ": " << specializations[i].name << "\n";
+    }
+
+    try {
+        std::cout << "\n> ";
+        int choice;
+        std::cin >> choice;
+
+        if (choice <= 0 or choice > num_of_specializations)
+            return -1;
     } catch (const std::exception &e) {
         return -1;
     }
