4a3138b6e7d27041ac72e80e5177695c0ec83e21
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-07-28 00:11:55 +0300

    feat(system): add system header file

---
+++ include/system.hpp
@@ -0,0 +1,30 @@
+#ifndef SYSTEM_HPP_
+#define SYSTEM_HPP_
+
+#include "specialization.hpp"
+#include <string>
+
+/**
+ * @brief Initialize Specializations
+ *
+ * @param specialization_names Specialization names
+ * @param specializations Specializations array
+ * @param num_of_specializations number of specializations
+ * @param specialization_capacity Specialization patients capacity
+ * @return int Returns 0 if success, -1 otherwise
+ */
+int init_specializations(std::string specialization_names[],
+                         Specialization specializations[],
+                         int num_of_specializations,
+                         int specialization_capacity);
+
+/**
+ * @brief Run System
+ *
+ * @param specializations Specializations array
+ * @param num_of_specializations number of specializations
+ * @return int Returns 0 if success, -1 otherwise
+ */
+int run_system(Specialization specializations[], int num_of_specializations);
+
+#endif // SYSTEM_HPP_
